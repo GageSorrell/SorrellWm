f9a946ef2e6a15b66be5843de93433085d1f07f5
Gage Sorrell <gage@sorrell.sh>
2026-05-02 12:20:58 -0400

    Tweaks to ts-tag query types.

--- Package/Tag/Source/Query/Query.Types.ts
+++ Package/Tag/Source/Query/Query.Types.ts
@@ -5,53 +5,67 @@
  * @license   MIT
  */
 
-export type TagMatchMode = "Exact" | "IncludingDescendants";
+export type TagMatchMode =
+    | "Exact"
+    | "IncludingDescendants";
 
-export type CountOperator = "Exactly" | "AtLeast" | "AtMost" | "Between";
+export type CountOperator =
+    | "Exactly"
+    | "AtLeast"
+    | "AtMost"
+    | "Between";
 
-export type AlwaysTagQuery = {
-    readonly Kind: "Always";
-};
+export type AlwaysTagQuery =
+    Readonly<{
+        Kind: "Always";
+    }>;
 
-export type NeverTagQuery = {
-    readonly Kind: "Never";
-};
+export type NeverTagQuery =
+    Readonly<{
+        Kind: "Never";
+    }>;
 
-export type HasTagQuery<TagType extends string> = {
-    readonly Kind: "Has";
-    readonly Tag: TagType;
-    readonly Match: TagMatchMode;
-};
+export type HasTagQuery<TagType extends string> =
+    Readonly<{
+        Kind: "Has";
+        Tag: TagType;
+        Match: TagMatchMode;
+    }>;
 
-export type NotTagQuery<TagType extends string> = {
-    readonly Kind: "Not";
-    readonly Query: TagQuery<TagType>;
-};
+export type NotTagQuery<TagType extends string> =
+    Readonly<{
+        Kind: "Not";
+        Query: TagQuery<TagType>;
+    }>;
 
-export type AndTagQuery<TagType extends string> = {
-    readonly Kind: "And";
-    readonly Queries: readonly TagQuery<TagType>[];
-};
+export type AndTagQuery<TagType extends string> =
+    Readonly<{
+        Kind: "And";
+        Queries: ReadonlyArray<TagQuery<TagType>>;
+    }>;
 
-export type OrTagQuery<TagType extends string> = {
-    readonly Kind: "Or";
-    readonly Queries: readonly TagQuery<TagType>[];
-};
+export type OrTagQuery<TagType extends string> =
+    Readonly<{
+        Kind: "Or";
+        Queries: ReadonlyArray<TagQuery<TagType>>;
+    }>;
 
-export type CountTagQuery<TagType extends string> = {
-    readonly Kind: "Count";
-    readonly Tags: readonly TagType[];
-    readonly Match: TagMatchMode;
-    readonly Operator: CountOperator;
-    readonly Minimum?: number;
-    readonly Maximum?: number;
-};
+export type CountTagQuery<TagType extends string> =
+    Readonly<{
+        Kind: "Count";
+        Tags: ReadonlyArray<TagType>;
+        Match: TagMatchMode;
+        Operator: CountOperator;
+        Minimum?: number;
+        Maximum?: number;
+    }>;
 
-export type IsSubsetOfTagQuery<TagType extends string> = {
-    readonly Kind: "IsSubsetOf";
-    readonly Tags: readonly TagType[];
-    readonly Match: TagMatchMode;
-};
+export type IsSubsetOfTagQuery<TagType extends string> =
+    Readonly<{
+        Kind: "IsSubsetOf";
+        Tags: ReadonlyArray<TagType>;
+        Match: TagMatchMode;
+    }>;
 
 export type CoreTagQuery<TagType extends string> =
     | AlwaysTagQuery
@@ -66,34 +80,40 @@ export type TagQuery<TagType extends string> =
     | CoreTagQuery<TagType>
     | IsSubsetOfTagQuery<TagType>;
 
-export type TagQueryBuilderOptions = {
-    readonly Match?: TagMatchMode;
-};
+export type TagQueryBuilderOptions =
+    Readonly<{
+        Match?: TagMatchMode;
+    }>;
 
-export type CountTagQueryInput<TagType extends string> = {
-    readonly Tags: readonly TagType[];
-    readonly Match?: TagMatchMode;
-    readonly Operator: CountOperator;
-    readonly Minimum?: number;
-    readonly Maximum?: number;
-};
+export type CountTagQueryInput<TagType extends string> =
+    Readonly<{
+        Tags: ReadonlyArray<TagType>;
+        Match?: TagMatchMode;
+        Operator: CountOperator;
+        Minimum?: number;
+        Maximum?: number;
+    }>;
 
-export type TagContainerLike<TagType extends string> = {
-    readonly HasExact: (Tag: TagType) => boolean;
-    readonly GetExactTags: () => Iterable<TagType>;
+export type TagContainerLike<TagType extends string> =
+    Readonly<{
+        HasExact: (Tag: TagType) => boolean;
+        GetExactTags: () => Iterable<TagType>;
 
-    /**
-     * Optional optimization. If provided, this should mean:
-     * "the container has this exact tag or some descendant of it."
-     */
-    readonly Has?: (Tag: TagType) => boolean;
-};
+        /**
+         * Optional optimization. If provided, this should mean:
+         * "the container has this exact tag or some descendant of it."
+         */
+        Has?: (Tag: TagType) => boolean;
+    }>;
 
-export type TagContainer<TagType extends string> = TagContainerLike<TagType> & {
-    readonly ToArray: () => readonly TagType[];
-};
+export type TagContainer<TagType extends string> =
+    TagContainerLike<TagType> &
+    Readonly<{
+        ToArray: () => ReadonlyArray<TagType>;
+    }>;
 
-export type TagRegistryLike<TagType extends string> = {
-    readonly GetAllTags: () => Iterable<TagType>;
-};
+export type TagRegistryLike<TagType extends string> =
+    Readonly<{
+        GetAllTags: () => Iterable<TagType>;
+    }>;
 
