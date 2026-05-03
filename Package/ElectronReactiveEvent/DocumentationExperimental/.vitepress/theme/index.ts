9494adec4ac3a9c3e232dff67f2ccd6a7e436d2d
Gage Sorrell <gage@sorrell.sh>
2026-05-03 14:46:43 -0400

    Adds new documentation site.

---
+++ Package/ElectronReactiveEvent/DocumentationExperimental/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @file      index.ts
+ * @author    Gage Sorrell <gage@sorrell.sh>
+ * @copyright (c) 2026 Gage Sorrell
+ * @license   MIT
+ */
+
+import { VPLTheme } from '@lando/vitepress-theme-default-plus';
+
+export default VPLTheme;
